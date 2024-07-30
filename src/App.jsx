7a446ab5e236aeb1cebd06acb16899db22671345
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-30 11:25:30 +0200

    implemented onchange functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
-
+import SearchBar from './components/searchbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState({
+    checked:false,
+    value:'',
+    delete:false
+  })
+  let containerArr=[]
+  
+  function handleChange(e){
 
+    const {value}= e.target
+setSearch(prev=>({
+  ...prev,
+  value:value
+}))
+ containerArr.push(search)
+  }
+  function addItems(){
+
+  }
+// let renderedItems=search.map()
   return (
     <>
-     
+     <h2 className='text-center font-medium text-8xl text-gray-300'>todos</h2>
+     <SearchBar
+    handleChange={handleChange}
+    addItems={addItems}
+    // name={heading}
+     />
+{console.log(containerArr)}
     </>
   )
 }
---
+++ src/components/searchbar.jsx
@@ -0,0 +1,15 @@
+const SearchBar = (props) => {
+  return (
+    <div className="flex justify-center mt-3 ">
+      <input
+        onChange={props.handleChange}
+        type="text"
+        placeholder="Add todo..."
+        className="text-black relative pl-6 border-2 rounded-3xl shadow-lg shadow-slate-500 w-[50%] py-3"
+      />
+      <i onClick={props.addItems} className="fa-solid fa-circle-plus absolute right-[27%] top-[16%] text-[#58d0ac] text-2xl"></i>
+    </div>
+  );
+};
+
+export default SearchBar;
