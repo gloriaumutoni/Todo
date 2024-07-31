35df5cb5016d52b4b3d98272565a09abad3fd027
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-31 09:14:26 +0200

    adding items on the page when you press enter

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import SearchBar from "./components/searchbar";
 import Items from "./components/items";
 import { nanoid } from "nanoid";
 
 function App() {
-  const [search, setSearch] = useState({
-    id : nanoid(),
-    checked: false,
-    title: "",
-    delete: false,
-    
-  });
-
-  const [todo, settodo] = useState([]);
-  
-  const [data, setData] = useState([search]);
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([]);
   function handleChange(e) {
-    const { value } = e.target.value;
-    setData((prevItem) =>
-      prevItem.map((prev) => ({
-        ...prev,
-        title: value,
-      }))
-    );
+    setInput(e.target.value);
   }
-  useEffect(() => {
-    data.map((items) => localStorage.setItem("items", JSON.stringify(items)));
-  }, [data]);
-
-  function addItems() {
-    // //  let localStorage.getItem(JSON.parse('items'))
-    // let renderItem= data.map(obj=> <Items key={id} value={obj.value}/>)
-    settodo([...todo,data.map(item=>item.title)])
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodo((prevTodo) => [
+      ...prevTodo,
+      { id: nanoid(), isChecked: false, title: input, deleted: false },
+    ]);
+    setInput("");
   }
 
   return (
     <>
       <h2 className="text-center font-medium text-8xl text-gray-200">todos</h2>
-      <SearchBar handleChange={handleChange} addItems={addItems} />
-      {/* {data && addItems()} */}
-      {todo.map((obj) => (
-        <Items key={obj.id} title={obj.title} />
-      ))}
+      <form onSubmit={handleSubmit}>
+        <SearchBar handleChange={handleChange} addItems={handleSubmit} />
+        {todo.map((items) => (
+          <Items key={items.id} title={items.title} />
+        ))}
+      </form>
     </>
   );
 }
