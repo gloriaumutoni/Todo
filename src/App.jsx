b48785aaeb679c78c224d83eef6c2661cc83e110
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-31 10:04:08 +0200

    deleting items

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,35 @@ function App() {
     e.preventDefault();
     setTodo((prevTodo) => [
       ...prevTodo,
-      { id: nanoid(), isChecked: false, title: input, deleted: false },
+      { id: nanoid(), isChecked: false, title: input, deleted: false},
     ]);
     setInput("");
   }
-
+// function handleChecked(e){
+// setTodo(prevTodo=>(
+//   prevTodo.map(todo=>{
+//     if(todo.isChecked){
+//       let styles
+// todo.tile?''
+//     }
+//     todo.isChecked?'line-through':'none'
+//   })
+// ))
+// }
+function handleDelete(id){
+  let newTodo=todo.filter(obj=>obj.id!==id)
+  setTodo(newTodo)
+}
   return (
     <>
       <h2 className="text-center font-medium text-8xl text-gray-200">todos</h2>
       <form onSubmit={handleSubmit}>
         <SearchBar handleChange={handleChange} addItems={handleSubmit} />
         {todo.map((items) => (
-          <Items key={items.id} title={items.title} />
+          <Items key={items.id} 
+          // isChecked={handleChecked} 
+          title={items.title}
+           delete={()=>handleDelete(items.id)}/>
         ))}
       </form>
     </>
--- src/components/items.jsx
+++ src/components/items.jsx
@@ -3,12 +3,14 @@ const Items = (props) => {
     <div className="flex justify-center">
       <div className="flex justify-between mt-10 w-[50%] border-b-2 pb-4">
         <div className="flex">
-          <input type="checkbox" />
+          <input type="checkbox" 
+          onClick={props.isChecked}
+          />
           <div className="text-2xl px-4">{props.title}</div>
         </div>
 
         <div className="text-end bg-slate-200 rounded-full w-10 mx-5">
-          <i className="fa-solid fa-trash text-[#f90b3b] text-center pr-3" />
+          <i onClick={props.delete} className="fa-solid fa-trash text-[#f90b3b] text-center pr-3" />
         </div>
       </div>
     </div>
